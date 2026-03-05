807118cb1117995a0c2ccb852abeed1151f5c959
andOrlando <bennettgillig@gmail.com>
2026-03-05 17:29:59 -0500

    instruction decode

--- rtl/processor.sv
+++ rtl/processor.sv
@@ -1,3 +1,21 @@
-module processor();
 
-endmodule; // processor
+module processor #(parameter N=4) (
+  input wire clk,
+  input [9+20*N:0] word
+);
+
+  wire [N*4-1:0] ops;
+  wire [N*8-1:0] As, Bs;
+
+  wire [1:0] branchop;
+  wire [7:0] branchaddr;
+
+  instruction_decode #(.N(N)) decode (.word(word), .ops(ops), .As(As), .Bs(Bs), .branchop(branchop), .branchaddr(branchaddr));
+
+  always @(posedge clk) begin
+  
+  end
+
+endmodule
+
+
