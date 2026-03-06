ac72fa68ead70364837d217914ba1fff3741f407
theokrueger <me@theokrueger.dev>
2026-03-06 01:22:49 -0500

    bump

--- rtl/slice/slice.sv
+++ rtl/slice/slice.sv
@@ -1,6 +1,5 @@
 module slice #(parameter N=0)
     (
-        input        CLK,
         input [3:0]  Instruction,
         input [7:0]  Target,
         input [7:0]  Arg1,
@@ -9,15 +8,74 @@ module slice #(parameter N=0)
         output [7:0] PC_out
     );
 `include "incl/ISA_Ops.svh"
+`include "incl/ALU_Ops.svh"
+`include "incl/Branch_Ops.svh"
 
     generate
         if (N == 0) begin
             // TODO hook up branch unit to alu
+            wire [3:0] branch_op;
+            always @(Instruction) begin
+                if (Instruction == ISA_OP_JEZ) begin
+
+                end
+                else if (Instruction == ISA_OP_JGZ) begin
+
+                end
+                //               else if (Instruction == ISA_OP_
+                else begin
+                    branch_op <= BRANCH_NO_OP;
+                end
+
+            end
             //branch_unit bu();
         end;
     endgenerate
 
-    always @(posedge CLK) begin
-
+    always @(Instruction or Target or Arg1 or Arg2 or PC_in) begin
+        case (Instruction)
+            ISA_OP_NOP:  begin
+                //TODO
+            end
+            ISA_OP_ADD:  begin
+                //TODO
+            end
+            ISA_OP_SUB:  begin
+                //TODO
+            end
+            ISA_OP_MUL:  begin
+                //TODO
+            end
+            ISA_OP_DIV:  begin
+                //TODO
+            end
+            ISA_OP_JMP:  begin
+                //TODO
+            end
+            ISA_OP_JEZ:  begin
+                //TODO
+            end
+            ISA_OP_JGZ:  begin
+                //TODO
+            end
+            ISA_OP_LADD: begin
+                //TODO
+            end
+            ISA_OP_LSUB: begin
+                //TODO
+            end
+            ISA_OP_LMUL: begin
+                //TODO
+            end
+            ISA_OP_LDIV: begin
+                // TODO
+            end
+            default: begin
+                $display("Unreachable case in branch_unit was reached!");
+                $finish;
+            end
+        endcase // case (Instruction)
     end
+
+end
 endmodule // slice
