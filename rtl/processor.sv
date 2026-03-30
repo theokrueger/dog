b379b962c0dde057d46c6979dee905baa4dd401c
andOrlando <bennettgillig@gmail.com>
2026-03-30 09:00:22 -0400

    fixes processor

--- rtl/processor.sv
+++ rtl/processor.sv
@@ -1,27 +1,19 @@
 module processor #(parameter N=4) (
-        input wire clk,
-        input [9+20*N:0] word
-    );
+  input wire clk,
+  input [9+20*N:0] word,
+  output [7:0] PC
+);
 
-    logic [7:0] PC;
-    logic [9+20*N:0] instructions;
+  wire [N*4-1:0] ops;
+  wire [N*8-1:0] As, Bs, Cs;
+  instruction_decode #(.N(N)) decode (.word(word), .ops(ops), .As(As), .Bs(Bs), .Cs(Cs), .branchop(branchop), .branchaddr(branchaddr));
 
-    logic [N*4-1:0] ops;
-    logic [N*8-1:0] As, Bs, Cs;
-
-    logic [1:0] branchop;
-    logic [7:0] branchaddr;
-
-    // TODO commented by theo because it causes build errors lmfao
-
-    //instruction_decode #(.N(N)) decode (.word(word), .ops(ops), .As(As), .Bs(Bs), .Cs(Cs) .branchop(branchop), .branchaddr(branchaddr));
-
-    //  genvar    i;
-    // generate
-    //   for (i=0; i<N; i++) begin
-    //     slice #(.N(N)) s (.CLK(clk), .Instruction(ops[8*i +: 8]), .Target(Cs[8*i +: 8]), .Arg1(As[8*i +: 8]), .Arg2(8*i +: 8), .PC_in(PC), .PC_out(PC))
-    //   end
-    // endgenerate
+  genvar i;
+  generate
+    for (i=0; i<N; i=i+1) begin
+      slice #(.N(N)) s (.CLK(clk), .Instruction(ops[4*i +: 4]), .Target(Cs[8*i +: 8]), .Arg1(As[8*i +: 8]), .Arg2(Bs[8*i +: 8]), .PC_in(PC), .PC_out(PC));
+    end
+  endgenerate
 
 endmodule
 
