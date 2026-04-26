9d2377fed9b0fc36f4aae75454a1e9d812c42b8c
theokrueger <me@theokrueger.dev>
2026-04-26 19:37:10 -0400

    poke

--- rtl/processor.sv
+++ rtl/processor.sv
@@ -15,28 +15,27 @@ module processor #(parameter N=2, parameter Regs=16) (
   wire [7:0] branchaddr;
   instruction_decode #(.N(N)) decode (.word(word), .ops(ops), .As(As), .Bs(Bs), .Cs(Cs), .branchop(branchop), .branchaddr(branchaddr));
 
-  wire [7:0] regs_out [Regs];
-  wire [7:0] write_data [N];
-  wire [reg_bits-1:0] write_sel [N];
+  wire [7:0] regs_out [0:Regs-1];
+  wire [7:0] write_data [0:N-1];
+  wire [reg_bits-1:0] write_sel [0:N-1];
   register_file #(.N(N), .Regs(Regs)) rf (.clk(clk), .rst(rst), .regs_out(regs_out), .write_data(write_data), .write_sel(write_sel));
 
   genvar i;
   wire [7:0] next_PC;
   generate
     for (i=0; i<N; i=i+1) begin
-      logic [7:0] A_reg, B_reg;
+       logic [7:0] A_reg;
+       logic [7:0] B_reg;
       logic [3:0] op_reg;
 
       always_ff @(posedge clk) begin
         A_reg <= regs_out[As[8*i +: 4]];
         case(ops[4*i +: 4])
-          // immediate (literal) operations
           ALU_ADD_IM_OP,
           ALU_SUB_IM_OP,
           ALU_MUL_IM_OP,
           ALU_DIV_IM_OP,
           ALU_MOD_IM_OP : B_reg <= Bs[8*i +: 8];
-          // read from registers
           default       : B_reg <= regs_out[Bs[8*i +: 4]];
         endcase;
         op_reg <= ops[4*i +: 4];
