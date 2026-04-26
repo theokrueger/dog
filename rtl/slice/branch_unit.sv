4c4958d563803e4a499e951f4ca4ffafdd0ccbc5
theokrueger <me@theokrueger.dev>
2026-04-26 19:26:40 -0400

    poke

--- rtl/slice/branch_unit.sv
+++ rtl/slice/branch_unit.sv
@@ -3,37 +3,37 @@ module branch_unit(
         input [2:0]      Operation,
         input [7:0]      Address,
         input [7:0]      PC,
-        input logic       Zero,
-        input logic       Sub_UF,
-        output logic [7:0] PC_out
+        input       Zero,
+        input       Sub_UF,
+        output [7:0] PC_out
     );
 `include "incl/Branch_Ops.svh"
 
     always @(posedge CLK) begin
         PC_out = PC + 8'b00000001;
-        unique case (Operation)
-                   BRANCH_NO_OP: begin
-                   end
-                   BRANCH_ZERO_OP: begin
-                       if (Zero == 1)
-                           PC_out = Address;
-                   end
-                   BRANCH_NOTZERO_OP: begin
-                       if (Zero == 0)
-                           PC_out = Address;
-                   end
-                   BRANCH_SUBOVERFLOW_OP: begin
-                       if (Sub_UF == 1)
-                           PC_out = Address;
-                   end
-                   BRANCH_UNCONDITIONAL_OP: begin
-                       PC_out = Address;
-                   end
-                   default: begin
-                       $display("Unreachable case in branch_unit was reached at %d!", $time);
-                       $display("Op: ", Operation);
-                       $finish;
-                   end
-               endcase // case (Operation)
-           end
-       endmodule // branch_unit
+        case (Operation)
+            BRANCH_NO_OP: begin
+            end
+            BRANCH_ZERO_OP: begin
+                if (Zero == 1)
+                    PC_out = Address;
+            end
+            BRANCH_NOTZERO_OP: begin
+                if (Zero == 0)
+                    PC_out = Address;
+            end
+            BRANCH_SUBOVERFLOW_OP: begin
+                if (Sub_UF == 1)
+                    PC_out = Address;
+            end
+            BRANCH_UNCONDITIONAL_OP: begin
+                PC_out = Address;
+            end
+            default: begin
+                $display("Unreachable case in branch_unit was reached at %d!", $time);
+                $display("Op: ", Operation);
+                $finish;
+            end
+        endcase // case (Operation)
+    end
+endmodule // branch_unit
