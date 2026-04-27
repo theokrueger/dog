5e27891a23e87257cfa28414d2b33441ffaf03d7
theokrueger <me@theokrueger.dev>
2026-04-27 01:24:15 -0400

    rpt fix

--- pnr/reporting_pnr.tcl
+++ pnr/reporting_pnr.tcl
@@ -1,13 +1,12 @@
 # drc
 verify_drc > $RPT_DIR/drc.rpt
 
-# report timing
 setAnalysisMode -analysisType onChipVariation
 timeDesign -postRoute > $RPT_DIR/timeDesign_summary.rpt
 report_timing         > $RPT_DIR/timing.rpt
 
-# report power
 reportPower > $RPT_DIR/power.rpt
 
-# report routing
 reportRoute > $RPT_DIR/route.rpt
+
+report_area > $RPT_DIR/area_report.rpt
